d4b555d16eb19f00d84809c7feeb5f1f51538051
Saurabh-yadav07 <syadav9718910607@gmail.com>
2026-02-05 23:57:02 +0530

    Updated the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,33 @@
+import { Card } from "react-bootstrap";
+
 function About() {
   return (
-    < div className="container my-5">
-      <h1>About Us</h1>
-      <p>
-        This e-commerce app is built using React, React Router, and Context API.
-      </p>
+    <div className="container my-5">
+      <Card className="p-4 shadow-sm">
+        <h1 className="mb-3 text-center">About Us</h1>
+
+        <p>
+          This e-commerce application is built using <strong>React</strong>,
+          <strong> React Router</strong>, and <strong> Context API</strong>.
+        </p>
+
+        <p>
+          The store page displays a list of products. Each product has its own
+          <strong> separate product details page</strong>, created using
+          <strong> dynamic routing</strong>.
+        </p>
+
+        <p>
+          Users can view multiple product images, read customer reviews, and
+          add products to the cart for a better shopping experience.
+        </p>
+
+        <p className="mb-0">
+          The cart functionality is managed globally using the
+          <strong> Context API</strong>, ensuring smooth state management across
+          the application.
+        </p>
+      </Card>
     </div>
   );
 }
